b4352302e114037f6bc3ea14a96b038610932140
Rodrigo Flores <ing.rodrigo.flores@gmail.com>
2021-12-01 23:22:51 -0600

    Detectando IP

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello world Python"
+    user_ip=request.remote_addr
+    return "Hello world Python, tu IP es: {}".format(user_ip)
