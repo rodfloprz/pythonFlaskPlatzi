92c68585a408f49450d790a7fd573a11a30fba05
Rodrigo Flores <ing.rodrigo.flores@gmail.com>
2021-12-01 23:30:39 -0600

    Detectando IP y ruteando

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-from flask import Flask, request
+from flask import Flask, request, redirect, make_response
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+def index():
     user_ip=request.remote_addr
+    response=make_response(redirect('/hello'))
+    response.set_cookie('user_ip',user_ip)
+
+    return response
+
+@app.route('/hello')
+def hello():
+    user_ip=request.cookies.get('user_ip')
     return "Hello world Python, tu IP es: {}".format(user_ip)
